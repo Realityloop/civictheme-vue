4a88eb3bd864165a041fb63d992109d510f6b8f8
Stuart Clark <stu@rtclark.net>
2023-01-30 08:53:12 +1100

    chore: linting fix

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -144,7 +144,7 @@ export default {
   },
 
   computed: {
-    hasImage: ({ imageSrc, $scopedSlots }) => imageSrc || $scopedSlots.image
+    hasImage: ({ $scopedSlots, imageSrc }) => imageSrc || $scopedSlots.image
   }
 }
 </script>

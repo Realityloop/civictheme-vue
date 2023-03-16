11d8c7914fdeb12f290a39e4e1b1a421cadf1da2
Stuart Clark <stu@rtclark.net>
2023-03-16 12:00:44 +1100

    feat(#21): add item list component (#22)
    * feat(#21): add item list component
    
    * chore(#21): update tests

---
+++ src/components/ItemList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="ct-item-list"
+    :class="{
+      [`ct-item-list--${type}`]: true,
+      'ct-item-list--no-gutter': noGutter
+    }">
+    <ul class="ct-item-list__list">
+      <li
+        v-for="(item, delta) of items"
+        :key="`ct-item-list--${delta}`"
+        class="ct-item-list__list__item">
+        <slot v-bind="{ item, delta }">
+          {{ item }}
+        </slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    noGutter: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'vertical'
+    }
+  }
+}
+</script>

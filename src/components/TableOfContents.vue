4beb77d9911bd7b38232ddfbb67c7a6c6065dacc
Stuart Clark <stu@rtclark.net>
2023-04-20 13:37:53 +1000

    feat(#27): add initial table of contents component

---
+++ src/components/TableOfContents.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="ct-table-of-contents"
+    :class="themeClass"
+  >
+    <div v-if="title" class="ct-table-of-contents__title" v-text="title" />
+    <ul class="ct-table-of-contents__links">
+      <li
+        v-for="item of items"
+        :key="`cct-table-of-contents__link-item--${item.id}`"
+        class="ct-table-of-contents__link-item">
+        <a class="ct-table-of-contents__link" :href="`#${item.id}`">
+          {{ item.title }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ThemeMixin from '../mixins/theme'
+
+export default {
+  mixins: [ThemeMixin],
+
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    title: {
+      type: String,
+      default: undefined
+    }
+  }
+}
+</script>

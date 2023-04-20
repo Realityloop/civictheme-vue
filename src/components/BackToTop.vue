f9208fa415c2b1c26ac1009eb229627ef00d25ae
Stuart Clark <stu@rtclark.net>
2023-04-21 08:14:26 +1000

    feat(#28): add back to top component (#29)
    * feat(#28): add back to top component
    
    * chore(#28): fix linting issues

---
+++ src/components/BackToTop.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="ct-back-to-top"
+    data-component-name="back-to-top"
+    data-scrollspy=""
+    data-scrollspy-offset="400"
+  >
+    <CTButton
+      kind="link"
+      :theme="theme"
+      url="#top"
+    >
+      <CTIcon
+        class="ct-button__icon"
+        symbol="up-arrow"
+        :theme="theme"
+      />
+    </CTButton>
+  </div>
+</template>
+
+<script>
+import ThemeMixin from "../mixins/theme"
+
+export default {
+  mixins: [ThemeMixin],
+
+  created() {
+    try {
+      if (process.client) require('civictheme/components/00-base/scrollspy/scrollspy')
+    }
+    catch(e) {
+      // eslint-disable-next-line
+      console.error(e)
+    }
+  }
+}
+</script>

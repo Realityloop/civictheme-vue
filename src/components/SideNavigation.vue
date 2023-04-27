f08135395c8086b910fe05c5bead2448b5ee699a
Stuart Clark <stu@rtclark.net>
2023-04-27 11:07:31 +1000

    feat(#36): add side navigation component (#37)

---
+++ src/components/SideNavigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="ct-side-navigation"
+    :class="themeClass"
+  >
+    <div
+      class="ct-skip-link"
+      :class="themeClass"
+    >
+      <a
+        class="ct-link ct-skip-link__link ct-visually-hidden ct-focusable"
+        :class="themeClass"
+        href="#main-content"
+        title="Skip to main content"
+      >
+        Skip to main content
+      </a>
+    </div>
+
+    <CTHeading v-if="title" class="ct-side-navigation__title" :text="title" />
+
+    <CTMenu class="ct-side-navigation__menu" :items="items" />
+  </div>
+</template>
+
+<script>
+import ThemeMixin from '../mixins/theme'
+
+export default {
+  mixins: [ThemeMixin],
+
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: undefined
+    }
+  }
+}
+</script>

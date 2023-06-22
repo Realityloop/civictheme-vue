8cf77f7e170bb48eb8042dca349cd725ece3f903
Stuart Clark <stu@rtclark.net>
2023-06-22 13:47:29 +1000

    chore(#34): fix test

--- test/components/Banner.cy.js
+++ test/components/Banner.cy.js
@@ -1,11 +1,15 @@
 /* global cy, describe, it */
 import Banner from '../../src/components/Banner.vue'
+import CTHeading from '../../src/components/Heading.vue'
+
+const components = { CTHeading }
 
 describe('Organisms/Banner', () => {
   it('Default', () => {
     cy.standardComponentTest(Banner, {
       axeRules: [],
       mountOptions: {
+        components,
         propsData: {
           title: 'Title',
           decorative: true

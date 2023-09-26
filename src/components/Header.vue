d7c6abec2a69f604d5c890fbf70f0c6940486b46
Stuart Clark <stu@rtclark.net>
2023-09-27 08:42:01 +1000

    feat(#31): make column classes configurable

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,7 @@
     :class="`ct-header ${themeClass}`"
     role="banner"
   >
+    <!-- Top -->
     <div
       v-if="hasTop"
       class="ct-header__top"
@@ -13,7 +14,7 @@
         class="container"
       >
         <div class="row">
-          <div class="col-xxs-12">
+          <div :class="columnClasses.top[0]">
             <div class="ct-header__content-top1">
               <div class="story-slot story-slot--content_top1">
                 <slot name="content_top1" />
@@ -28,13 +29,13 @@
         class="container"
       >
         <div class="row">
-          <div class="col-xxs-12 col-m-6">
+          <div :class="columnClasses.top[1]">
             <div class="ct-header__content-top2">
               <slot name="content_top2" />
             </div>
           </div>
 
-          <div class="col-xxs-12 col-m-6">
+          <div :class="columnClasses.top[2]">
             <div class="ct-header__content-top3">
               <slot name="content_top3" />
             </div>
@@ -51,7 +52,7 @@
     >
       <div class="container">
         <div class="row">
-          <div class="col-xxs-12">
+          <div :class="columnClasses.middle[0]">
             <div class="ct-header__content-middle1">
               <div class="story-slot story-slot--content_middle1">
                 <slot name="content_middle1" />
@@ -63,13 +64,13 @@
 
       <div class="container">
         <div class="row">
-          <div class="col-xxs-6">
+          <div  :class="columnClasses.middle[1]">
             <div class="ct-header__content-middle2">
               <slot name="content_middle2" />
             </div>
           </div>
 
-          <div class="col-xxs-6">
+          <div  :class="columnClasses.middle[2]">
             <div
               class="
                 ct-header__content-middle3
@@ -91,7 +92,7 @@
     >
       <div class="container">
         <div class="row">
-          <div class="col-xxs-12">
+          <div  :class="columnClasses.bottom[0]">
             <div class="ct-header__content-bottom1">
               <slot name="content_bottom1" />
             </div>
@@ -109,6 +110,10 @@ export default {
   mixins: [ThemeMixin],
 
   props: {
+    columns: {
+      type: Object,
+      default: undefined
+    },
     topClasses: {
       type: String,
       default: undefined
@@ -124,6 +129,24 @@ export default {
   },
 
   computed: {
+    columnClasses: ({ columns }) => {
+      const colClasses = (o) => Object.entries(o).map(([key, value]) => `col-${key}-${value}`).join(' ')
+      return {
+        top: [
+          colClasses(((columns || {}).top || [])[0] || { xxs: 12 }),
+          colClasses(((columns || {}).top || [])[1] || { xxs: 12, m: 6 }),
+          colClasses(((columns || {}).top || [])[2] || { xxs: 12, m: 6 }),
+        ],
+        middle: [
+          colClasses(((columns || {}).middle || [])[0] || { xxs: 12 }),
+          colClasses(((columns || {}).middle || [])[1] || { xxs: 6 }),
+          colClasses(((columns || {}).middle || [])[2] || { xxs: 6 }),
+        ],
+        bottom: [
+          colClasses(((columns || {}).bottom || [])[0] || { xxs: 12 }),
+        ],
+      }
+    },
     hasTop: ({ $scopedSlots }) => $scopedSlots.content_top1 || $scopedSlots.content_top2 || $scopedSlots.content_top3,
     hasMiddle: ({ $scopedSlots }) => $scopedSlots.content_middle1 || $scopedSlots.content_middle2 || $scopedSlots.content_middle3,
     hasBottom: ({ $scopedSlots }) => $scopedSlots.content_bottom1

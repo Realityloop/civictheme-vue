69200732fbfe1c60564aa3f98b139f9ae7664290
Stuart Clark <stu@rtclark.net>
2023-10-23 13:45:05 +1100

    feat(#106): emit click event

--- src/components/Collapsible.vue
+++ src/components/Collapsible.vue
@@ -5,7 +5,7 @@
       'data-collapsible-collapsed': collapsed
     }"
   >
-    <div data-collapsible-trigger="">
+    <div data-collapsible-trigger="" @click="onClick">
       <slot name="trigger">{{ trigger }}</slot>
     </div>
     <div data-collapsible-panel="">
@@ -29,19 +29,32 @@ export default {
 
   mounted() {
     try {
+      // If the component has been mounted and $el is available
       if (this.$el) {
+        // Delete the cached module for collapsible
         delete require.cache[require.resolve('civictheme/components/00-base/collapsible/collapsible')]
+
+        // Require the collapsible module again
         require('civictheme/components/00-base/collapsible/collapsible')
       }
     }
     catch(e) {
+      // Output an error message to the console if there is an issue
       // eslint-disable-next-line
       console.error(e)
     }
   },
 
+  // Delete require cache for collapsible component
   beforeDestroy() {
     delete require.cache[require.resolve('civictheme/components/00-base/collapsible/collapsible')]
+  },
+
+  methods: {
+    onClick() {
+      // Emit the 'click' event to notify parent components that the trigger has been clicked.
+      this.$emit('click')
+    }
   }
 }
 </script>

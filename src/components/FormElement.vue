831f59e84c669c3487580be0e2d89d75f3906346
Stuart Clark <stu@rtclark.net>
2023-10-31 15:46:35 +1100

    chore(#13): cleanup wrapper elements

--- src/components/FormElement.vue
+++ src/components/FormElement.vue
@@ -1,80 +1,74 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xxs-12">
-        <div
-          :class="`ct-form-element ${themeClass} ct-form-element--${type} ct-form-element--label--undefined ct-form-element--label--after`"
-        >
-          <!-- Label: before -->
-          <label
-            v-if="isLabelBefore"
-            :class="`ct-label ${themeClass} ct-label--regular`"
-            :for="`input-${id}`"
-            v-text="label"
-          />
+  <div
+    :class="`ct-form-element ${themeClass} ct-form-element--${type} ct-form-element--label--undefined ct-form-element--label--after`"
+  >
+    <!-- Label: before -->
+    <label
+      v-if="isLabelBefore"
+      :class="`ct-label ${themeClass} ct-label--regular`"
+      :for="`input-${id}`"
+      v-text="label"
+    />
 
-          <div
-            class="ct-input ct-input--default"
-            :class="{
-              [themeClass]: true,
-              [`ct-input--${type}`]: true,
-              'ct-input--required required': required
-            }"
-          >
-            <!-- Textfield: text, email, tel, password -->
-            <CTTextfield
-              v-if="['date', 'email', 'number', 'password', 'tel', 'text'].includes(type)"
-              v-bind="{ id: `input-${id}`, placeholder, required, type }"
-              v-model="model"
-            />
+    <div
+      class="ct-input ct-input--default"
+      :class="{
+        [themeClass]: true,
+        [`ct-input--${type}`]: true,
+        'ct-input--required required': required
+      }"
+    >
+      <!-- Textfield: text, email, tel, password -->
+      <CTTextfield
+        v-if="['date', 'email', 'number', 'password', 'tel', 'text'].includes(type)"
+        v-bind="{ id: `input-${id}`, placeholder, required, type }"
+        v-model="model"
+      />
 
-            <!-- Textarea -->
-            <CTTextarea
-              v-else-if="type === 'textarea'"
-              v-bind="{ id: `input-${id}`, placeholder, required }"
-              v-model="model"
-            />
+      <!-- Textarea -->
+      <CTTextarea
+        v-else-if="type === 'textarea'"
+        v-bind="{ id: `input-${id}`, placeholder, required }"
+        v-model="model"
+      />
 
-            <!-- Select -->
-            <CTSelect
-              v-else-if="type === 'select'"
-              v-bind="{ error: !!error, id: `input-${id}`, options, placeholder, required }"
-              v-model="model"
-            />
+      <!-- Select -->
+      <CTSelect
+        v-else-if="type === 'select'"
+        v-bind="{ error: !!error, id: `input-${id}`, options, placeholder, required }"
+        v-model="model"
+      />
 
-            <!-- Radio -->
-            <CTRadio
-              v-else-if="type === 'radio'"
-              v-bind="{ id: `input-${id}`, placeholder, required }"
-              v-model="model"
-            />
+      <!-- Radio -->
+      <CTRadio
+        v-else-if="type === 'radio'"
+        v-bind="{ id: `input-${id}`, placeholder, required }"
+        v-model="model"
+      />
 
-            <!-- Checkbox -->
-            <CTCheckbox
-              v-else-if="type === 'checkbox'"
-              v-bind="{ id: `input-${id}`, label, placeholder, required }"
-              v-model="model"
-            />
-          </div>
-
-          <!-- Label: after -->
-          <label
-            v-if="isLabelAfter"
-            :class="`ct-label ${themeClass} ct-label--regular`"
-            :for="`input-${id}`"
-            v-text="label"
-          />
-
-          <div
-            v-if="description"
-            class="ct-form-element__description"
-            v-text="description"
-          />
-
-          <CTFieldMessage v-if="$slots.error || error" type="error"><slot name="error">{{ error }}</slot></CTFieldMessage>
-        </div>
-      </div>
+      <!-- Checkbox -->
+      <CTCheckbox
+        v-else-if="type === 'checkbox'"
+        v-bind="{ id: `input-${id}`, label, placeholder, required }"
+        v-model="model"
+      />
     </div>
+
+    <!-- Label: after -->
+    <label
+      v-if="isLabelAfter"
+      :class="`ct-label ${themeClass} ct-label--regular`"
+      :for="`input-${id}`"
+      v-text="label"
+    />
+
+    <div
+      v-if="description"
+      class="ct-form-element__description"
+      v-text="description"
+    />
+
+    <CTFieldMessage v-if="$slots.error || error" type="error"><slot name="error">{{ error }}</slot></CTFieldMessage>
   </div>
 </template>
 

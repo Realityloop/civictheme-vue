a8cdb62f74ac2780d542be7cfa5e7aaf574a90d4
Stuart Clark <stu@rtclark.net>
2024-01-16 15:01:40 +1100

    chore(#49): fix typo

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -109,7 +109,7 @@ export default {
       if (!url) return false
       try {
         const urlObj = new URL(url)
-        if (typeof window !== undefiend && window?.location?.host === urlObj?.host) return false
+        if (typeof window !== 'undefined' && window?.location?.host === urlObj?.host) return false
         return ['http:', 'https:'].includes(urlObj?.protocol)
       } catch(err) { return false }
     },
